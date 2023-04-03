98daa48bf8e4d8cc980bafaf462409875b7ea1b7
tjjoris <tjjoris@gmail.com>
2023-04-03 11:05:29 -0400

    first step of pathfinding getting adjacent tiles, and storing them in this step list

--- Assets/Systems/Pathfinding/PathfindingManager.cs
+++ Assets/Systems/Pathfinding/PathfindingManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using GameJam.Map;
+using UnityEngine.Tilemaps;
 
 namespace GameJam.Pathfinding
 {
@@ -9,9 +10,33 @@ namespace GameJam.Pathfinding
     {
         [SerializeField] private List<Vector3Int> _tilesExplored;
         [SerializeField] private List<Vector3Int> _tilesInThisStep;
+        private MapManager _mapManager;
+        [SerializeField] private Tilemap _map;
+
+        private void Awake()
+        {
+            _mapManager = GetComponent<MapManager>();
+        }
+        
         public void FillPathInfinate(Vector3Int sourceCoords)
         {
-
+            Vector3Int[] adjacentTiles = _mapManager.GetAllAdjacentHexCoordinates(sourceCoords);
+            foreach (Vector3Int tileCoordChecking in adjacentTiles)
+            {
+                if (IsTileNotExplored(tileCoordChecking)) 
+                {
+                    if (!_tilesInThisStep.Contains(tileCoordChecking))
+                    {
+                        _tilesInThisStep.Add(tileCoordChecking);
+                    }
+                }
+                
+            }
+        }
+        private bool IsTileNotExplored(Vector3Int sourceCoords)
+        {
+            // if (_map.)
+            return true;
         }
     }
 }
