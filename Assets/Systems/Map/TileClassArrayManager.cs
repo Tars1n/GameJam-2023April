edd3488968d915d8ae1790bcc711601a3925e6f4
tjjoris <tjjoris@gmail.com>
2023-04-04 19:05:36 -0400

    delete check if class at cord instance for each function, and remove the check in the class creation

--- Assets/Systems/Map/TileClassArrayManager.cs
+++ Assets/Systems/Map/TileClassArrayManager.cs
@@ -39,29 +39,25 @@ namespace GameJam.Map
                         continue;
                     }    
                     Debug.Log($"coords " + coords);
-                    CheckIfClassAtCoord(coords);
+                    CheckIfClassAtCoordAndCreate(coords);
                 }
             }
             if (_debugLog) Debug.Log($"tile nodes array " + _tileNodesArray);
         }
         public Vector3Int GetPreviousStepCoord(Vector3Int coord)
         {
-            CheckIfClassAtCoord(coord);
             return _tileNodesArray[coord.x, coord.y].PreviousStepGridPosition;
         }
         public void SetPreviousStepCoord(Vector3Int coord, Vector3Int previousCoord)
         {
-            CheckIfClassAtCoord(coord);
             _tileNodesArray[coord.x, coord.y].PreviousStepGridPosition = previousCoord;
         }
         public void SetPreviousStepCoordToItself(Vector3Int coord)
         {
-            CheckIfClassAtCoord(coord);
             _tileNodesArray[coord.x, coord.y].PreviousStepGridPosition = coord;
         }
         public void RemoveEntityAtCoord(Vector3Int coord, GameObject entity)
         {
-            CheckIfClassAtCoord(coord);
             if (_tileNodesArray[coord.x, coord.y].Entities.Contains(entity))
             {
                 _tileNodesArray[coord.x, coord.y].Entities.Remove(entity);
@@ -69,22 +65,18 @@ namespace GameJam.Map
         }
         public void SetEndityAtCoord(Vector3Int coord, GameObject entity)
         {
-            CheckIfClassAtCoord(coord);
             _tileNodesArray[coord.x, coord.y].Entities.Add(entity);
         }
         public List<GameObject> GetEntitiesAtCoord(Vector3Int coord, int yCoord)
         {
             return _tileNodesArray[coord.x, coord.y].Entities;
         }
-        public void CheckIfClassAtCoord(Vector3Int coord)
+        public void CheckIfClassAtCoordAndCreate(Vector3Int coord)
         {
-            if (_tileNodesArray[coord.x, coord.y] == null)
-            {
                 TileNode tileNode = new TileNode();
                 _tileNodesArray[coord.x, coord.y] = tileNode;
                 _tileNodesArray[coord.x, coord.y].PreviousStepGridPosition = coord;//set the prev step to itself
                 _tileNodesArray[coord.x, coord.y].GridPosition = coord;
-            }
         }
     }
 
