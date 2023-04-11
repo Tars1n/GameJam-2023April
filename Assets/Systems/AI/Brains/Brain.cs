b7024eb6b8019c2c7c153562405f0201d664bcd6
tjjoris <tjjoris@gmail.com>
2023-04-11 12:00:25 -0400

    Created Brain folder and abstract brain

---
+++ Assets/Systems/AI/Brains/Brain.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam
+{
+    [System.Serializable]
+    public abstract class Brain
+    {
+    }
+}
