8bdb1e601d072a79d079ae86ab985a7513627b12
tjjoris <tjjoris@gmail.com>
2023-04-11 16:25:52 -0400

    brain loop trying through loop

--- Assets/Systems/AI/Brains/BrainLoopActivities.cs
+++ Assets/Systems/AI/Brains/BrainLoopActivities.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GameJam.Map;
+using GameJam.Entity;
 
 namespace GameJam.Entity.Brain
 {
@@ -9,13 +11,34 @@ namespace GameJam.Entity.Brain
     {
         [SerializeField] private List<Activity> _activitiesToLoop;
         public List<Activity> ActivitiesToLoop => _activitiesToLoop;
+        [SerializeField] private int _stepInActivityLoop;
+        private MapManager _mapManager;
+        private ReferenceManager _ref;
+        private EntityBase _entityBase;
         private void Awake()
         {
-            _activitiesToLoop = new List<Activity>();
+            _activitiesToLoop = new List<Activity>();     
+            _entityBase = GetComponent<EntityBase>();      
+        }
+        private void Start() 
+        {
+            _ref = GameMaster.Instance.ReferenceManager;
+            _mapManager = _ref.MapManager;
         }
         public override void Think()
         {
-
+            Vector3Int axialToMoveTo = _mapManager.CastOddRowToAxial(_entityBase.CurrentTileNode.GridCoordinate);
+            axialToMoveTo += _activitiesToLoop[_stepInActivityLoop].GridCoord;
+            Debug.Log($"move to coords " + axialToMoveTo);
+            IncreaseStep();
+        }
+        private void IncreaseStep()
+        {
+            _stepInActivityLoop ++;
+            if (_stepInActivityLoop >= _activitiesToLoop.Count)
+            {
+                _stepInActivityLoop = 0;
+            }
         }
     }
 }
