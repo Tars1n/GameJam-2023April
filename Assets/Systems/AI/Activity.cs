96858b226ffaa5393d76c095b6926e9b49ce7d13
tjjoris <tjjoris@gmail.com>
2023-04-11 11:48:15 -0400

    created Activity Class

---
+++ Assets/Systems/AI/Activity.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam.Entity.Brain
+{
+    [System.Serializable]
+    public class Activity
+    {
+        
+    }
+}
