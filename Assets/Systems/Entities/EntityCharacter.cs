8c2a15b32997eb1a95ac760e6dbff6adc8d83aca
Tars1n <mudd.rosal@gmail.com>
2023-04-14 09:54:48 -0400

    EntityCharacter stores mirror references

--- Assets/Systems/Entities/EntityCharacter.cs
+++ Assets/Systems/Entities/EntityCharacter.cs
@@ -6,6 +6,10 @@ namespace GameJam.Entity
 {
     public class EntityCharacter : EntityBase
     {
+        [SerializeField] private EntityCharacter _mirrorEntityX;
+        public EntityCharacter MirrorEntityX => _mirrorEntityX;
+        [SerializeField] private EntityCharacter _mirrorEntityY;
+        public EntityCharacter MirrorEntityY => _mirrorEntityY;
         
 
         public override void DoTurnAction()
