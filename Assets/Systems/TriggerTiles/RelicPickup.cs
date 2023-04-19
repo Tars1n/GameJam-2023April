e3572d992a0a973c1cd17e55db1799047b637683
tjjoris <tjjoris@gmail.com>
2023-04-19 11:19:17 -0400

    Can gather relic

--- Assets/Systems/TriggerTiles/RelicPickup.cs
+++ Assets/Systems/TriggerTiles/RelicPickup.cs
@@ -7,8 +7,17 @@ namespace GameJam.Map.TriggerTiles
 {
     public class RelicPickup : TriggerTileManager
     {
+        [SerializeField] private int _relicsGathered;
+
         public override void EntityEnteredTrigger(EntityBase entityBase, TileNode tileNode)
         {
+            if (_triggerLocationTiles == null) return;
+                if (tileNode == null)
+                {
+                    Debug.LogWarning($"Attempting to set TriggerTile out of bounds: {tileNode}");
+                }
+                tileNode.ClearTrigger();
+                _relicsGathered ++;
         }
     }
 }
