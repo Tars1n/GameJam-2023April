c796cca0d1da564691b662a7519ef2a5daa3427a
tjjoris <tjjoris@gmail.com>
2023-04-19 10:51:41 -0400

    Created Relic Pickup script

---
+++ Assets/Systems/TriggerTiles/RelicPickup.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameJam.Entity;
+
+namespace GameJam.Map.TriggerTiles
+{
+    public class RelicPickup : TriggerTileManager
+    {
+        public override void EntityEnteredTrigger(EntityBase entityBase, TileNode tileNode)
+        {
+        }
+    }
+}
