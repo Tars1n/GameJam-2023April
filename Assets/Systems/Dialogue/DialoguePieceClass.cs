ba5dcc8d5ef718b08aefa70f26910b24663ebe17
tjjoris <tjjoris@gmail.com>
2023-04-26 16:50:16 -0400

    Created dialoguePieceClass, and DialoguePieceTextClass which inherits from DialoguePieceClass
    I know the names could be better

---
+++ Assets/Systems/Dialogue/DialoguePieceClass.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam.Dialogue
+{   
+    [System.Serializable]
+    public abstract class DialoguePieceClass
+    {
+        
+    }
+}
---
+++ Assets/Systems/Dialogue/DialoguePieceTextClass.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameJam.Dialogue
+{
+    [System.Serializable]
+    public class DialoguePieceTextClass : DialoguePieceClass
+    {
+        [SerializeField] private string _dialogueText;
+        public string DialogueTextStr => _dialogueText;
+    }
+}
