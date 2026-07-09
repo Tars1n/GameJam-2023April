943b68447a8ec42cc4762a3ee5caa9780579f04c
tjjoris <tjjoris@gmail.com>
2026-07-08 21:27:00 -0400

    edited HilightManager.cs, added check to do nothing if passed canMovePos == saved canMovePos, also added setting the saved canMovePos to null for HideCanMoveTile(), also improved function comment headers.

--- Assets/Systems/Map/HilightManager.cs
+++ Assets/Systems/Map/HilightManager.cs
@@ -22,12 +22,15 @@ namespace GameJam
         }
 
         /**
-if the saved hilight coordinates are not null renders the hilight tile at that coordinate null.
-then saves the passed coordinates into the saved hilight coordinates
+        first checks if the passed hilight tile equals the current hilight tile, if they do, does nothing,
+then checks if the saved hilight coordinates are not null, if so s
+removes the hilight from the rendered map by setting that tile to null.
+then saves the passed hilight coordinates into the saved hilight coordinates
 then reners the saved hilight coordinates to hilighted on the map.
         **/
         public void RenderCanMoveTile(Vector3Int canMovePos)
         {
+            if (this.canMovePos == canMovePos) return;
             if (this.canMovePos != null)
             {
                 hilightMap.SetTile(this.canMovePos.Value, null);
@@ -43,6 +46,7 @@ then reners the saved hilight coordinates to hilighted on the map.
             if (this.canMovePos != null)
             {
                 hilightMap.SetTile(this.canMovePos.Value, null);
+                this.canMovePos = null;
             }
         }
     }
